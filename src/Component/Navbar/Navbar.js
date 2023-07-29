95303c2fed496416f1376ec134526b214d24205e
M Bilal Naeem <119736324+Bilal-Naeem12@users.noreply.github.com>
2023-07-29 16:58:09 +0500

    navbar problem fixed

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -22,13 +22,7 @@ export default class Navbar extends Component {
   active =  () => {
 
     let url = document.URL;
-    if (url === "http://localhost:3000/") {
-      this.setState({
-
-
-        types: ""
-      })
-    }else{
+    
     let parts = url.split('/');
 
    this.setState({
@@ -36,13 +30,13 @@ export default class Navbar extends Component {
 
       types: parts[parts.length - 1]
     })
-  }
+  
   }
   render() {
     return (
       <nav className="navbar navbar-expand-lg bg-dark navbar-dark sticky-top">
         <div className="container">
-          <Link className="navbar-brand " to="/"><img src="https://logos-download.com/wp-content/uploads/2016/11/IMDb_logo_logotype.png" alt="" /></Link>
+          <Link className={`navbar-brand ${this.state.types === "" ? "" : ""}`} onClick={this.componentDidMount} to="/"><img src="https://logos-download.com/wp-content/uploads/2016/11/IMDb_logo_logotype.png" alt="" /></Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
